b05e7410f95659e93ba56bdc9069aab641a826fd
githubuser102938 <dreamstar151012@gmail.com>
2024-06-30 21:07:42 +0530

    Added new file and updated existing content

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/routes.js';
+import DBconnection from './database/db.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use ('/', router);
+
+const PORT = 8000;
+
+DBconnection();
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
